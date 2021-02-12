c46b922dc73c68707b69c7eb96957e239135fe49
NAGA LAKSHMI PRASANNA RAAVI <naga.lakshmi.prasanna.raavi@ibm.com>
2021-02-12 12:06:03 +0530

    Anew file abcde

---
+++ Abc.java
@@ -0,0 +1,9 @@
+public class Abc
+{
+public static void main(String args[])
+{ for(int i=1;i<10;i++)
+{
+System.out,println("hello");
+System.out.println("hihihi");
+
+}}}
---
+++ Abcde.java
@@ -0,0 +1,6 @@
+public class Abcde
+{
+public static void main(String args[])
+{
+        System.out.println("Hello, World!"); 
+}}
